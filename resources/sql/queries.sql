38962433f4b89376d56f6980ff3f3d05b7c31469
Christian Egli <christian.egli@sbs.ch>
2021-08-30 11:06:55 +0200

    Remove unnecessary query for local words
    since either uncontracted or contracted should always be non-null we
    do not need to query for either being non-null in the case of grade 0

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -98,8 +98,8 @@ AND hyphenation.spelling =
   FROM  documents_document
   WHERE id = :id)
 WHERE words.document_id = :id
--- either uncontracted or contracted should always be non-null so the following should be implicitely the case and hence not needed
--- (when (= (:grade params) 0) "AND (words.uncontracted IS NOT NULL OR words.contracted IS NOT NULL)")
+-- either uncontracted or contracted should always be non-null so no
+-- need to query for either being non-null in the case of grade 0
 --~ (when (= (:grade params) 1) "AND words.uncontracted IS NOT NULL")
 --~ (when (= (:grade params) 2) "AND words.contracted IS NOT NULL")
 --~ (when (:search params) "AND words.untranslated LIKE :search")
