ebe56f212d235119ca6faa0b40a09014364766ef
Christian Egli <christian.egli@sbs.ch>
2023-06-15 12:01:08 +0200

    Move the preview code to a separate namespace

--- src/cljs/daisyproducer2/documents/document.cljs
+++ src/cljs/daisyproducer2/documents/document.cljs
@@ -2,6 +2,7 @@
   (:require [ajax.core :as ajax]
             [clojure.string :as string]
             [daisyproducer2.documents.state :as state]
+            [daisyproducer2.documents.preview :as preview]
             [daisyproducer2.i18n :refer [tr]]
             [daisyproducer2.words.grade :as grade]
             [daisyproducer2.words.local :as local]
@@ -71,42 +72,6 @@
    #_[:button.button.is-success
     (tr [:transitions-state] [(-> document :state-id state/next-mapping state/mapping)])]])
 
-(defn- tooltip-button [{:keys [tooltip icon href label] :as opts}]
-  ;; if we have an href we need an anchor element. Otherwise use a button
-  (let [element (if href :a.button.has-tooltip-arrow :button.button.has-tooltip-arrow)]
-    [element
-     (merge
-      {:data-tooltip (tr [tooltip])
-       :aria-label (tr [tooltip])}
-      (dissoc opts :tooltip :icon))
-     [:span.icon {:aria-hidden true}
-      [:i.mi {:class icon}]]
-     (when label [:span (tr [label])])]))
-
-(defn- label-button [{:keys [label icon href] :as opts}]
-  ;; if we have an href we need an anchor element. Otherwise use a button
-  (let [element (if href :a.button :button.button)]
-    [element
-     (dissoc opts :label :icon)
-     [:span.icon {:aria-hidden true}
-      [:i.mi {:class icon}]]
-     (when label [:span (tr [label])])]))
-
-(defn preview-links [document]
-  [:div.blockxo
-   [:table.table
-    [:tbody
-     ;; FIXME: not implemented yet
-     #_[:tr
-      [:th (tr [:braille])]
-      [:td [:div.field.is-grouped
-            [:p.control [tooltip-button {:tooltip :download :icon "mi-download"}]]]]]
-     [:tr
-      [:th (tr [:epub3])]
-      [:td [:div.field.is-grouped
-            [:p.control [tooltip-button {:tooltip :download :icon "mi-download"}]]
-            [:p.control [label-button {:label :open-in-online-player :icon "mi-open-in-new" }]]]]]]]])
-
 (defn page []
   (let [document @(rf/subscribe [::current])]
     [:section.section>div.container>div.content
@@ -135,5 +100,4 @@
     [:section.section>div.container>div.content
      [summary document]
      [tabs document]
-     [preview-links document]]))
-
+     [preview/preview-links document]]))
---
+++ src/cljs/daisyproducer2/documents/preview.cljs
@@ -0,0 +1,47 @@
+(ns daisyproducer2.documents.preview
+  (:require [ajax.core :as ajax]
+            [clojure.string :as string]
+            [daisyproducer2.documents.state :as state]
+            [daisyproducer2.i18n :refer [tr]]
+            [daisyproducer2.words.grade :as grade]
+            [daisyproducer2.words.local :as local]
+            [daisyproducer2.words.unknown :as unknown]
+            [re-frame.core :as rf]))
+
+(defn- tooltip-button [{:keys [tooltip icon href label] :as opts}]
+  ;; if we have an href we need an anchor element. Otherwise use a button
+  (let [element (if href :a.button.has-tooltip-arrow :button.button.has-tooltip-arrow)]
+    [element
+     (merge
+      {:data-tooltip (tr [tooltip])
+       :aria-label (tr [tooltip])}
+      (dissoc opts :tooltip :icon))
+     [:span.icon {:aria-hidden true}
+      [:i.mi {:class icon}]]
+     (when label [:span (tr [label])])]))
+
+(defn- label-button [{:keys [label icon href] :as opts}]
+  ;; if we have an href we need an anchor element. Otherwise use a button
+  (let [element (if href :a.button :button.button)]
+    [element
+     (dissoc opts :label :icon)
+     [:span.icon {:aria-hidden true}
+      [:i.mi {:class icon}]]
+     (when label [:span (tr [label])])]))
+
+(defn preview-links [document]
+  [:div.block
+   [:table.table
+    [:tbody
+     ;; FIXME: not implemented yet
+     #_[:tr
+      [:th (tr [:braille])]
+      [:td [:div.field.is-grouped
+            [:p.control [tooltip-button {:tooltip :download :icon "mi-download"}]]]]]
+     [:tr
+      [:th (tr [:epub3])]
+      [:td [:div.field.is-grouped
+            [:p.control [tooltip-button {:tooltip :download :icon "mi-download"}]]
+            [:p.control [label-button {:label :open-in-online-player :icon "mi-open-in-new" }]]]]]]]])
+
+
