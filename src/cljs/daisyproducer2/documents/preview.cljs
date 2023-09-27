e01cedca575843a5643f28b5e2e8cb9bc41c1ffb
Christian Egli <christian.egli@sbs.ch>
2023-09-27 16:07:19 +0200

    Implement client side of ebook download and open in player
    The download is done with a simple link that opens in a new window.
    The open in player does a first request to generate the EPUB, gets the
    location of the resource and then programatically opens the resource
    in a new window

--- src/cljs/daisyproducer2/documents/preview.cljs
+++ src/cljs/daisyproducer2/documents/preview.cljs
@@ -6,55 +6,72 @@
             [daisyproducer2.words.grade :as grade]
             [daisyproducer2.words.local :as local]
             [daisyproducer2.words.unknown :as unknown]
+            [daisyproducer2.words.notifications :as notifications]
             [re-frame.core :as rf]))
 
-(defn- tooltip-button [{:keys [tooltip icon href label] :as opts}]
-  ;; if we have an href we need an anchor element. Otherwise use a button
-  (let [element (if href :a.button.has-tooltip-arrow :button.button.has-tooltip-arrow)]
-    [element
-     (merge
-      {:data-tooltip (tr [tooltip])
-       :aria-label (tr [tooltip])}
-      (dissoc opts :tooltip :icon))
-     (when label [:span (tr [label])])
-     [:span.icon {:aria-hidden true}
-      [:i.mi {:class icon}]]]))
+(rf/reg-event-fx
+  ::generate-epub
+  (fn [{:keys [db]} [_ id product-id]]
+    {:db (notifications/set-button-state db id :generate-epub)
+     :http-xhrio {:method          :get
+                  :uri             (str "api/documents/" id "/preview/epub-in-player")
+                  :params          {:product-id product-id}
+                  :response-format (ajax/json-response-format {:keywords? true})
+                  :on-success      [::ack-generate-epub id]
+                  :on-failure      [::ack-failure id :generate-epub]
+                  }}))
 
-(defn- label-button [{:keys [label icon href] :as opts}]
-  ;; if we have an href we need an anchor element. Otherwise use a button
-  (let [element (if href :a.button :button.button)]
-    [element
-     (dissoc opts :label :icon)
-     (when label [:span (tr [label])])
-     [:span.icon {:aria-hidden true}
-      [:i.mi {:class icon}]]]))
+(rf/reg-event-fx
+  ::ack-generate-epub
+  (fn [{:keys [db]} [_ id {location :location}]]
+    {:db (notifications/clear-button-state db id :generate-epub)
+     :dispatch [::redirect-online-player location]}))
 
-(defn preview-links [document]
+(rf/reg-event-db
+ ::ack-failure
+ (fn [db [_ id request-type response]]
+   (-> db
+       (assoc-in [:errors request-type] (or (get-in response [:response :status-text])
+                                            (get response :status-text)))
+       (notifications/clear-button-state id request-type))))
+
+(rf/reg-event-fx
+  ::redirect-online-player
+  (fn [{:keys [db]} [_ location]]
+    {:db db
+     :open-window location}))
+
+(rf/reg-fx
+  :open-window
+  (fn [uri]
+    (.open js/window uri "_blank")))
+
+(defn preview-links [{id :id}]
   [:div.block
-    [:table.table
-     [:thead
-      [:tr
-       [:th {:width "100%"} (tr [:format])]
-       [:th (tr [:action])]]]
-     [:tbody
-      #_[:tr
-       [:th (tr [:braille])]
-       [:td [:div.field.is-grouped
-             [:p.control [tooltip-button {:tooltip :download :icon "mi-download"}]]]]]
-      [:tr
-       [:th (tr [:epub3])]
-       [:td [:div.field.is-grouped
-             [:p.control [label-button {:label :download :icon "mi-download"}]]
-             [:p.control [label-button {:label :online-player :icon "mi-open-in-new" }]]]]]
-      #_[:tr
-       [:th (tr [:large-print])]
-       [:td [:div.field.is-grouped
-             [:p.control [label-button {:label "Library" :icon "mi-download"}]]
-             [:p.control [label-button {:label "Sale" :icon "mi-download"}]]
-             [:p.control [label-button {:label "Configurable" :icon "mi-download"}]]]]]
-      #_[:tr
-       [:th (tr [:open-document "Open Document"])]
-       [:td [:div.field.is-grouped
-             [:p.control [tooltip-button {:tooltip :download :icon "mi-download"}]]]]]]]])
+     [:table.table
+      [:thead
+       [:tr
+        [:th {:width "100%"} (tr [:format])]
+        [:th (tr [:action])]]]
+      [:tbody
+       [:tr
+        [:th (tr [:epub3])]
+        [:td [:div.field.is-grouped
+              [:p.control
+               [:a.button
+                {:href (str "/api/documents/" id "/preview/epub")
+                 ;;:download "download"
+                 :target "_blank"}
+                [:span (tr [:download])]
+                [:span.icon {:aria-hidden true}
+                 [:i.mi.mi-download]]]]
+              [:p.control
+               (let [loading? @(rf/subscribe [::notifications/button-loading? id :generate-epub])]
+                 [:button.button
+                  {:class (when loading? "is-loading")
+                   :on-click #(rf/dispatch [::generate-epub id "EB12951"])}
+                  [:span (tr [:online-player])]
+                  [:span.icon {:aria-hidden true}
+                   [:i.mi.mi-open-in-new]]])]]]]]]])
 
 
