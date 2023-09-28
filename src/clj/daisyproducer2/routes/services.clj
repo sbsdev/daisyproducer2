f20ecda412c6e64174a76fb9a421d1a0df2c6cfe
Christian Egli <christian.egli@sbs.ch>
2023-09-28 11:00:22 +0200

    Drop the api endpoint to fetch products for a document
    It is not needed. The only reason this is remotely interesting is to
    give the produced EPUBs a name that can be connected to the internal
    product number. But really the client doesn't need to know anything
    about products.

--- src/clj/daisyproducer2/routes/services.clj
+++ src/clj/daisyproducer2/routes/services.clj
@@ -108,18 +108,7 @@
              :handler (fn [{{{:keys [id]} :path} :parameters}]
                         (if-let [doc (db/get-document {:id id})]
                           (ok doc)
-                          (not-found)))}}]
-     ["/products"
-      {:get {:summary "Get all products for a document. Optionally limit the result by `type`"
-             :parameters {:path {:id int?}
-                          :query {(spec/opt :type) int?}}
-             :handler (fn [{{{:keys [id]} :path
-                             {:keys [type]} :query} :parameters}]
-                        (if (nil? type)
-                          (ok (db/get-products {:document_id id}))
-                          (if-let [product (db/get-products {:document_id id :type type})]
-                            (ok product)
-                            (not-found))))}}]]]
+                          (not-found)))}}]]]
 
    ["/words"
     {:swagger {:tags ["Global Words"]}}
