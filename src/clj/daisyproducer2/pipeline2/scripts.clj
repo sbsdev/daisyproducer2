59e9984e49f8597e9f0ad1745e3bace09618913d
Christian Egli <christian.egli@sbs.ch>
2023-11-02 11:42:23 +0100

    Add the dtbook-to-odt script

--- src/clj/daisyproducer2/pipeline2/scripts.clj
+++ src/clj/daisyproducer2/pipeline2/scripts.clj
@@ -4,7 +4,8 @@
   specific scripts."
   (:require [clojure.string :as string]
             [clojure.java.io :as io]
-            [daisyproducer2.pipeline2.core :as pipeline2]))
+            [daisyproducer2.pipeline2.core :as pipeline2]
+            [clojure.set :as set]))
 
 (defn validate [input & {:keys [mathml-version check-images] :as opts}]
   (pipeline2/create-job-and-wait "dtbook-validator" [input] [] (merge opts {:input-dtbook input})))
@@ -21,4 +22,23 @@
                   out (io/output-stream epub)]
         (io/copy in out)))))
 
-
+(defn dtbook-to-odt [dtbook images odt
+                     {:keys [asciimath phonetics image-handling line-numbers page-numbers page-numbers-float answer]
+                      :or {asciimath "BOTH"
+                           phonetics true
+                           image-handling "LINK"
+                           line-numbers true
+                           page-numbers true
+                           page-numbers-float false
+                           answer "_.."}
+                      :as opts}]
+  (pipeline2/with-job [job (pipeline2/job-create "sbs:dtbook-to-odt" [dtbook] images (set/rename-keys opts {:image_handling :images}))]
+    (let [completed (pipeline2/wait-for job)
+          results (pipeline2/get-results completed)
+          odt-stream (->> results
+                          (filter #(string/ends-with? % ".odt"))
+                          first
+                          pipeline2/get-stream)]
+      (with-open [in odt-stream
+                  out (io/output-stream odt)]
+        (io/copy in out)))))
