1831673482d7c05732bfa73070ae965d3863e9ae
Christian Egli <christian.egli@sbs.ch>
2024-02-15 16:43:34 +0100

    Add a FIXME to a code smell

--- src/cljs/daisyproducer2/documents/state.cljs
+++ src/cljs/daisyproducer2/documents/state.cljs
@@ -2,6 +2,8 @@
   (:require
    [daisyproducer2.i18n :refer [tr]]))
 
+;; FIXME: This is majorly brittle and fails as soon as the ids in the
+;; database change
 (def mapping {1 (tr [:new]) 7 (tr [:in-production]) 8 (tr [:finished])})
 (def next-mapping {1 7 ; new -> in production
                    7 8 ; in production -> finished
