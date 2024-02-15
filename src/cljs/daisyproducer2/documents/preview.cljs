503dd59c205fd4618602de968b3161a551be4fe1
Christian Egli <christian.egli@sbs.ch>
2024-02-15 16:43:10 +0100

    Clean up the require statement

--- src/cljs/daisyproducer2/documents/preview.cljs
+++ src/cljs/daisyproducer2/documents/preview.cljs
@@ -1,13 +1,5 @@
 (ns daisyproducer2.documents.preview
-  (:require [ajax.core :as ajax]
-            [clojure.string :as string]
-            [daisyproducer2.documents.state :as state]
-            [daisyproducer2.i18n :refer [tr]]
-            [daisyproducer2.words.grade :as grade]
-            [daisyproducer2.words.local :as local]
-            [daisyproducer2.words.unknown :as unknown]
-            [daisyproducer2.words.notifications :as notifications]
-            [re-frame.core :as rf]))
+  (:require [daisyproducer2.i18n :refer [tr]]))
 
 (defn preview-links [{id :id}]
   [:div.block
