49af0b238889560b911ace0c51d13405562d4f85
Christian Egli <christian.egli@sbs.ch>
2024-05-24 16:21:51 +0200

    Use a resource solver so that the validator finds schemas inside a jar

--- src/clj/daisyproducer2/documents/schema_validation.clj
+++ src/clj/daisyproducer2/documents/schema_validation.clj
@@ -6,6 +6,9 @@
            org.xml.sax.SAXException
            org.xml.sax.SAXParseException
            org.xml.sax.ErrorHandler
+           org.w3c.dom.ls.LSResourceResolver
+           org.w3c.dom.bootstrap.DOMImplementationRegistry
+           org.w3c.dom.ls.DOMImplementationLS
            javax.xml.validation.SchemaFactory
            javax.xml.transform.stream.StreamSource))
 
@@ -19,6 +22,35 @@
 (defn stringify
   [{:keys [error line]}]
   (format "%s on line %d" error line))
+
+(def resource-resolver
+  "A resolver that helps finding schema files inside a Jar file. The
+  `resolveResource` method of the `LSResourceresolver` interface
+  accepts a number of params about a given resource and returns a
+  `LSInput` object. This object will contain the proper `systemId` so
+  that the resource is found inside the Jar file."
+  (reify LSResourceResolver
+    (resolveResource [this type namespaceURI publicId systemId baseURI]
+      (let [;; the `systemId` contains the file name of the schema file we are
+            ;; looking for. Given that we construct the path to the resource in
+            ;; the Jar, create an io/resource and finally convert it to an URL.
+            url (->>
+                 systemId
+                 (str "schema/")
+                 io/resource
+                 io/as-url)
+            ;; `resolveResource` has to return an `LSInput` object that contains
+            ;; enough information about the resource so that it can be found. So
+            ;; we create an empty `LSInput` object and set the `systemId` to our
+            ;; `url`. But to get an empty `LSInput` we'll first have to create a
+            ;; `DOMImplementationRegistry`, then from that a `DOMImplementation`
+            ;; which can be used to create a new `LSInput`.
+            registry (DOMImplementationRegistry/newInstance)
+            implementation (.getDOMImplementation registry "Core 3.0 XML 3.0 LS")
+            input (doto (.createLSInput implementation)
+                    ;; once we have the `LSInput` we can set the `systemId`
+                    (.setSystemId (str url)))]
+        input))))
 
 (defn validation-errors
   "Return a list of validation errors when validating `file` against
@@ -39,14 +71,17 @@
                         (warning [this exception]))
         language XMLConstants/RELAXNG_NS_URI
         ;; We are using jing here because the plain validator (using
-        ;; W3C_XML_SCHEMA_NS_URI) doesn't seem to work properly. It
-        ;; claims that smil files are valid against the vubis bulk
-        ;; import schema.
+        ;; W3C_XML_SCHEMA_NS_URI) doesn't seem to work properly. It claims that
+        ;; smil files are valid against the vubis bulk import schema.
         factory "com.thaiopensource.relaxng.jaxp.XMLSyntaxSchemaFactory"
         schema-url (io/as-url (io/resource schema))
         validator (doto (.newValidator
                          (.newSchema
-                          (SchemaFactory/newInstance language factory nil)
+                          (doto (SchemaFactory/newInstance language factory nil)
+                            ;; we have to sett a resource resolver on the schema
+                            ;; factory so that it will find the (included)
+                            ;; schema files inside the Jar file
+                            (.setResourceResolver resource-resolver))
                           schema-url))
                     (.setErrorHandler error-handler))]
     (try
