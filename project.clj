3c213052425fe8c28d49019e2607b36744f420bc
Christian Egli <christian.egli@sbs.ch>
2024-06-18 15:08:57 +0200

    Revert back to an older version of Bulma
    as the colors seem not as nice in newer bulma and the tables aren't
    full width

--- project.clj
+++ project.clj
@@ -61,7 +61,7 @@
                  [org.daisy.bindings/jhyphen "1.0.2" :exclusions [net.java.dev.jna/jna]]
                  [org.liblouis/liblouis-java "5.1.0" :exclusions [net.java.dev.jna/jna]]
                  [org.tobereplaced/nio.file "0.4.0"]
-                 [org.webjars.npm/bulma "1.0.1"]
+                 [org.webjars.npm/bulma "0.9.4"]
                  [org.webjars.npm/creativebulma__bulma-tooltip "1.2.0"]
                  [org.webjars.npm/material-icons "1.13.2"]
                  [org.webjars/webjars-locator "0.52" :exclusions [org.slf4j/slf4j-api]]
