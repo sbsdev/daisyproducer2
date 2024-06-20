65a27333959a9cdc4eb7eb190ec083d345377153
Christian Egli <christian.egli@sbs.ch>
2024-06-20 14:42:51 +0200

    Improve a comment

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -67,8 +67,7 @@ SET state_id = (CASE :state WHEN "open" THEN 7 WHEN "closed" THEN 8 END)
 WHERE id = :id
 
 -- :name update-document-meta-data :! :n
--- :doc update the meta data of a document with given `id`.
--- Note: `publisher` and `language` cannot be updated
+-- :doc update the meta data of a document with given `id`. Note: `publisher` and `language` cannot be updated.
 UPDATE documents_document
 SET title = :title,
     author = :author,
