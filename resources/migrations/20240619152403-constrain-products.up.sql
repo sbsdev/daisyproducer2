6e2f8f153d1cd63bc9ca3c4eb4ded4e2bac0b80f
Christian Egli <christian.egli@sbs.ch>
2024-06-20 16:33:17 +0200

    Fix some whitespace and documentation typos

--- resources/migrations/20240619152403-constrain-products.up.sql
+++ resources/migrations/20240619152403-constrain-products.up.sql
@@ -1,14 +1,14 @@
--- (``product.document_id`, ``product.identifier`) should be unique
+-- (`product.document_id`, `product.identifier`) should be unique
 ALTER TABLE documents_product ADD CONSTRAINT UNIQUE product_document_id_identifier_unique (document_id, identifier);
 
 --;;
 
--- ``product.identifier` should be unique
+-- `product.identifier` should be unique
 ALTER TABLE documents_product ADD CONSTRAINT UNIQUE product_identifier_unique (identifier);
 
 --;;
 
--- (``product.type`, `product.document_id`) should be unique, except for type 0
+-- (`product.type`, `product.document_id`) should be unique, except for type 0
 -- really we want a partial index here, but that exists only in real databases so
 -- we have to resort to a workaround by using a virtual fake column, see
 -- https://stackoverflow.com/q/7804565 and in particular
