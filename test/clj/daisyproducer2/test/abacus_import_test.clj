6265dc4fbdc5e7686b3e132ee4f07f9cb329f4cd
Christian Egli <christian.egli@sbs.ch>
2024-07-04 16:04:12 +0200

    Fix a sample based test

--- test/clj/daisyproducer2/test/abacus_import_test.clj
+++ test/clj/daisyproducer2/test/abacus_import_test.clj
@@ -10,7 +10,7 @@
    [daisyproducer2.documents.abacus :refer :all]
    [java-time.api :as time]))
 
-(defrecord Raw [product-number title creator language source date source-publisher source-edition production-series-number reihe aufwand daisyproducer?])
+(defrecord Raw [product-number title creator language source date source-publisher source-edition production-series-number reihe aufwand daisy-producer])
 (defrecord Imported [product-number product-type
                      title author publisher date source language
                      source-publisher source-edition
@@ -32,7 +32,7 @@
 (def raw-gen (gen/tuple product-number gen/string gen/string language sbs-isbn date (gen/return "") (gen/return "") gen/nat reihe aufwand daisy_producer))
 
 (defn- xml-sample
-  [{:keys [product-number title creator source language date source-publisher source-edition production-series-number reihe aufwand daisyproducer?]}]
+  [{:keys [product-number title creator source language date source-publisher source-edition production-series-number reihe aufwand daisy-producer]}]
   [:AbaConnectContainer
    [:Task
     [:Transaction
@@ -51,7 +51,7 @@
         [:auflageJahr source-edition]
         [:rucksackNr production-series-number]
         [:reihe reihe]
-        [:daisy_producer daisyproducer?]
+        [:daisy_producer daisy-producer]
         [:Aufwand_A2 aufwand]]]]]]])
 
 (deftest abacus-import
@@ -61,20 +61,20 @@
       (let [document (->Imported "EB11111" :ebook "Eine für de Thesi" "Gwerder, Anna" "SBS Schweizerische Bibliothek für Blinde, Seh- und Lesebehinderte"
                                  (time/local-date "2011-12-23") "" "de" "DVA" "1. / 2011" "" "" "" false)]
         (are [expected actual] (= (into {} expected) (read-xml (xml/sexp-as-element (xml-sample actual))))
-          document (->Raw "EB11111" "Eine für de Thesi" "Gwerder, Anna" "de" "" "2011-12-23" "DVA" "1. / 2011" 0 "" "" false)
-          (assoc document :daisyproducer? true) (->Raw "EB11111" "Eine für de Thesi" "Gwerder, Anna" "de" "" "2011-12-23" "DVA" "1. / 2011" 0 "" "" true)
+          document (->Raw "EB11111" "Eine für de Thesi" "Gwerder, Anna" "de" "" "2011-12-23" "DVA" "1. / 2011" 0 "" "" "nein")
+          (assoc document :daisyproducer? true) (->Raw "EB11111" "Eine für de Thesi" "Gwerder, Anna" "de" "" "2011-12-23" "DVA" "1. / 2011" 0 "" "" "ja")
           (assoc document :production-series-number "500" :production-series "PPP")
-          (->Raw "EB11111" "Eine für de Thesi" "Gwerder, Anna" "de" "" "2011-12-23" "DVA" "1. / 2011" 500 "" "" false)
+          (->Raw "EB11111" "Eine für de Thesi" "Gwerder, Anna" "de" "" "2011-12-23" "DVA" "1. / 2011" 500 "" "" "nein")
           (assoc document :production-series-number "7000" :production-series "SJW")
-          (->Raw "EB11111" "Eine für de Thesi" "Gwerder, Anna" "de" "" "2011-12-23" "DVA" "1. / 2011" 0 "SJW 7000" "" false)
+          (->Raw "EB11111" "Eine für de Thesi" "Gwerder, Anna" "de" "" "2011-12-23" "DVA" "1. / 2011" 0 "SJW 7000" "" "nein")
           (assoc document :product-number "GD11111" :product-type :large-print)
-          (->Raw "GD11111" "Eine für de Thesi" "Gwerder, Anna" "de" "" "2011-12-23" "DVA" "1. / 2011" 0 "" "" false)
+          (->Raw "GD11111" "Eine für de Thesi" "Gwerder, Anna" "de" "" "2011-12-23" "DVA" "1. / 2011" 0 "" "" "nein")
           (assoc document :product-number "PS11111" :product-type :braille)
-          (->Raw "PS11111" "Eine für de Thesi" "Gwerder, Anna" "de" "" "2011-12-23" "DVA" "1. / 2011" 0 "" "" false)
+          (->Raw "PS11111" "Eine für de Thesi" "Gwerder, Anna" "de" "" "2011-12-23" "DVA" "1. / 2011" 0 "" "" "nein")
           (assoc document :production-source "electronicData")
-          (->Raw "EB11111" "Eine für de Thesi" "Gwerder, Anna" "de" "" "2011-12-23" "DVA" "1. / 2011" 0 "" "D" false)
+          (->Raw "EB11111" "Eine für de Thesi" "Gwerder, Anna" "de" "" "2011-12-23" "DVA" "1. / 2011" 0 "" "D" "nein")
           document
-          (->Raw "EB11111" "Eine für de Thesi" "Gwerder, Anna" "de" "" "2011-12-23" "DVA" "1. / 2011" 0 "" "C" false))))))
+          (->Raw "EB11111" "Eine für de Thesi" "Gwerder, Anna" "de" "" "2011-12-23" "DVA" "1. / 2011" 0 "" "C" "nein"))))))
 
 (deftest abacus-import-file
   (testing "ABACUS import"
