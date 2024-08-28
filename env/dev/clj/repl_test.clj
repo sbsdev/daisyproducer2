5b93a20f453635a0a8e6b861c5cb491bd00f7ea9
Christian Egli <christian.egli@sbs.ch>
2024-08-28 09:19:08 +0200

    Comment some test code that only works in very specific situations

--- env/dev/clj/repl_test.clj
+++ env/dev/clj/repl_test.clj
@@ -3,23 +3,23 @@
 
 ;; invoke the following expressions in a REPL inside the
 ;; daisyproducer2.documents.abacus namespace
-
-(documents/get-document 49)
-
-(versions/insert-initial-version (documents/get-document 49))
+(comment
+  (documents/get-document 49)
+  (versions/insert-initial-version (documents/get-document 49)))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Test the predicates ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(product-seen-before? "EB31506")
-(source-or-title-source-edition-seen-before? {:source "" :title "" :source-edition ""})
-(source-or-title-source-edition-seen-before? {:source "" :title "Übersetzungstest (MK)" :source-edition ""})
-(source-or-title-source-edition-seen-before? {:source "" :title "Übersetzungstest (MK)" :source-edition "2. / 2006"})
-(source-or-title-source-edition-seen-before? {:source "SBS362058" :title "" :source-edition ""})
-(source-or-title-source-edition-seen-before? (documents/get-document 49))
-(product-seen-before? "ET4728726")
+(comment
+  (product-seen-before? "EB31506")
+  (source-or-title-source-edition-seen-before? {:source "" :title "" :source-edition ""})
+  (source-or-title-source-edition-seen-before? {:source "" :title "Übersetzungstest (MK)" :source-edition ""})
+  (source-or-title-source-edition-seen-before? {:source "" :title "Übersetzungstest (MK)" :source-edition "2. / 2006"})
+  (source-or-title-source-edition-seen-before? {:source "SBS362058" :title "" :source-edition ""})
+  (source-or-title-source-edition-seen-before? (documents/get-document 49))
+  (product-seen-before? "ET4728726"))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Test update-document with unchanged metadata ;;
@@ -27,8 +27,9 @@
 
 ;; ensure that the document has an associated product
 
-(products/insert-product 49 "EB31506" 1)
-(update-document (assoc (documents/get-document 49) :product-number "EB31506"))
+(comment
+  (products/insert-product 49 "EB31506" 1)
+  (update-document (assoc (documents/get-document 49) :product-number "EB31506")))
 
 ;; make sure no new version was created
 
@@ -36,7 +37,8 @@
 ;; Test update-document with changed metadata ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(update-document (assoc (documents/get-document 49) :author "a test author" :product-number "EB31506"))
+(comment
+  (update-document (assoc (documents/get-document 49) :author "a test author" :product-number "EB31506")))
 
 ;; make sure the metadata is changed
 ;; select author from documents_document where id = 49;
@@ -51,7 +53,8 @@
 ;; Try to change the production-series metadata ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(update-document (assoc (documents/get-document 49) :production-series "PPP" :product-number "EB31506"))
+(comment
+  (update-document (assoc (documents/get-document 49) :production-series "PPP" :product-number "EB31506")))
 
 ;; make sure the metadata is changed
 ;; select author from documents_document where id = 49;
@@ -66,7 +69,8 @@
 ;; Try to change the source metadata ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(update-document (assoc (documents/get-document 49) :source "SBS362058" :product-number "EB31506"))
+(comment
+  (update-document (assoc (documents/get-document 49) :source "SBS362058" :product-number "EB31506")))
 
 ;; make sure the metadata is changed
 ;; select author from documents_document where id = 49;
@@ -81,7 +85,8 @@
 ;; Test update-document-and-product ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(update-document-and-product (assoc (documents/get-document 49) :product-number "ET4728726"))
+(comment
+  (update-document-and-product (assoc (documents/get-document 49) :product-number "ET4728726")))
 
 ;; make sure the metadata is _not_ changed
 ;; select author from documents_document where id = 49;
@@ -98,21 +103,22 @@
 ;; Test insert-document-and-product ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(insert-document-and-product
- {:production-series-number "6"
-  :source-publisher ""
-  :date (LocalDate/parse "2080-04-24")
- :production-series "PPP"
- :source-edition ""
- :publisher "SBS Schweizerische Bibliothek für Blinde, Seh- und Lesebehinderte"
- :product-number "PS86141"
- :source "33-508974-91-0"
- :title "Voilà"
- :author "Edgar Palmérise",
- :production-source "electronicData",
- :product-type :braille,
- :language "de",
- :daisyproducer? true})
+(comment
+  (insert-document-and-product
+   {:production-series-number "6"
+    :source-publisher ""
+    :date (LocalDate/parse "2080-04-24")
+    :production-series "PPP"
+    :source-edition ""
+    :publisher "SBS Schweizerische Bibliothek für Blinde, Seh- und Lesebehinderte"
+    :product-number "PS86141"
+    :source "33-508974-91-0"
+    :title "Voilà"
+    :author "Edgar Palmérise",
+    :production-source "electronicData",
+    :product-type :braille,
+    :language "de",
+    :daisyproducer? true}))
 
 ;; make sure a new document has been created
 ;; select * from documents_document where title = "Voilà";
