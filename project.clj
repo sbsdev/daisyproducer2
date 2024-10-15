053c043f5ffd7e2e6b1540a36654f366e70279b4
Christian Egli <christian.egli@sbs.ch>
2024-10-15 15:57:21 +0200

    Version 0.9.16-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ch.sbs/daisyproducer2 "0.9.15"
+(defproject ch.sbs/daisyproducer2 "0.9.16-SNAPSHOT"
 
   :description "FIXME: write description"
   :url "https://github.com/sbsdev/daisyproducer2"
