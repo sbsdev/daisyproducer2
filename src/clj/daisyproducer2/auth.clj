8e720a5cbba3dc9b71033b37b974d9cb8703549c
Christian Egli <christian.egli@sbs.ch>
2024-11-07 16:05:07 +0100

    Drop an unused function

--- src/clj/daisyproducer2/auth.clj
+++ src/clj/daisyproducer2/auth.clj
@@ -1,9 +1,9 @@
 (ns daisyproducer2.auth
-  (:require [buddy.sign.jwt :as jwt]
-            [clojure.tools.logging :as log]
-            [daisyproducer2.config :refer [env]]
-            [daisyproducer2.ldap :as ldap]
-            [daisyproducer2.utils :as utils]))
+  (:require
+   [buddy.sign.jwt :as jwt]
+   [clojure.tools.logging :as log]
+   [daisyproducer2.config :refer [env]]
+   [daisyproducer2.ldap :as ldap]))
 
 (defn login [username password]
   (if-let [user (ldap/authenticate username password)]
@@ -13,6 +13,3 @@
       {:token token
        :user user})
     (log/debug "Login failed for" username)))
-
-(defn is-admin? [{{user :user} :identity :as req}]
-  (utils/is-admin? user))
